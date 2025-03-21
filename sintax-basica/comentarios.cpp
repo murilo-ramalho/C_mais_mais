24cc3b301a4e1d96dd7b1e8e24045366e6d341fb
Murilo Ramalho <murilo.ramalho@proton.me>
2025-03-21 03:02:18 -0300

    terminado sintax basica

---
+++ sintax-basica/comentarios.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "teste de comentario" << std::endl;
+
+    // comentario de 1 linha
+    
+    /* 
+    
+       comentario de multiplas linhas
+
+    */
+
+    return 0;
+}
